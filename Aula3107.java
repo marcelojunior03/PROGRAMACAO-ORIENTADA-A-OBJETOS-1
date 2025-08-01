e76cd597eb88cf949b1c7e9148e291ac10ad979f
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-07-31 22:19:53 -0300

    Add files via upload

---
+++ Aula3107.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula3107;
+
+/**
+ *
+ * @author IFSP
+ */
+public class Aula3107 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        int x;
+        x = 20;
+        System.out.println(x);
+        
+        String nome = "Marcelo";
+        System.out.println("Meu nome é " + nome);
+        
+        final double pi = 3.14;
+        
+        double raio = 7;
+        double area = pi * (raio * raio);
+        
+        System.out.println("A área do círculo é " + area);
+        
+        //Isso é um comentário
+        /*
+            Sinais aritméticos
+            + : soma
+            - : subtração
+            * : multiplicação
+            / : divisão
+            = : atribuição
+        */
+    }
+    
+}
